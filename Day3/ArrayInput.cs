b600fd555f589a292a973300111898c9fe14d564
Nimap <Nimap@DESKTOP-RRJ6NCT>
2023-04-08 18:29:19 +0530

    Add project files.

---
+++ Day3/ArrayInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class ArrayInput
+    {
+        public static void Main3(string [] args)
+        {
+            Console.WriteLine("How many elements u want to add??");
+            int element = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[element];
+
+            for(int i = 0; i<element; i++)
+            {
+                Console.WriteLine("add element on " + (i + 1));
+                int data = Convert.ToInt32(Console.ReadLine());
+                arr[i] = data; 
+            }
+
+            Console.WriteLine("---------DATA---------");
+            foreach(int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        } 
+    }
+}
---
+++ Day3/Function2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Function2
+    {
+        public static void add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+        public static void Main6(string [] args)
+        {
+            Console.WriteLine("Enter the value of a and b:- ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Function2.add(a, b);
+
+        }
+    }
+}
